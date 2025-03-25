8e9b3ff7f6ca497bafa6c6caca0f3f3f2162f1e3
kein <kein10119810@gmail.com>
2025-03-26 07:36:44 +0900

    db

---
+++ login/sql/struts.sql
@@ -0,0 +1,17 @@
+set name utf8;
+set foreign_key_checks = 0;
+drop database if exists logindb_kudo;
+create database logindb_kudo;
+use logindb_kudo;
+
+create table user(
+id int,
+user_name varchar(225),
+password varchar(225)
+);
+
+insert into user values
+(1, "taro", "123"),
+(2, "jiro", "123"),
+(3, "hanako", "123"),
+(4, "saburo", "123");
